eb6c8d2c36a53dd71de7e4b2429144509e29d74d
Meeran Equebal <meeran.equebal@gmail.com>
2026-02-13 00:42:07 +0530

    tailwind of first searchList done

--- src/app/searchList.tsx
+++ src/app/searchList.tsx
@@ -20,7 +20,7 @@ function Component({ index, date, title, id, isMobile }: Component) {
         <div className="pointer-events-none w-[6vw] grow-0 pr-2 pl-2 md:pr-0 md:pl-0">
           {index}
         </div>
-        <div className="pointer-events-none h-[3rem] w-20 grow content-center overflow-hidden pr-2 text-ellipsis max-[780px]:h-10 max-[780px]:pl-2 md:pr-0">
+        <div className="pointer-events-none h-10 w-20 grow content-center overflow-hidden pr-2 pl-2 text-ellipsis md:h-[3rem] md:pr-0 md:pl-0">
           {title}
         </div>
         <div className="pointer-events-none w-56 grow-0 pr-2 pl-2 md:pr-0 md:pl-0">
